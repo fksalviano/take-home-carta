2d348c7f273deecb8219ac9969d38a3105b14532
Fabio Salviano <fksalviano@gmail.com>
2022-08-20 22:35:17 -0300

    refact: read all lines function and read file usecase

--- src/Vesting/Application/Commons/Utils/FileUtil.cs
+++ src/Vesting/Application/Commons/Utils/FileUtil.cs
@@ -1,19 +1,21 @@
+using System.Linq.Expressions;
+
 namespace Application.Commons.Utils;
 
 public static class FileUtil
 {
     public static async Task<IEnumerable<T>> ReadAllLines<T>(string fileName, CancellationToken cancellationToken, 
-        Func<string[], int, T> parseValuesFunc)
+        Func<string[], T> parseValuesFunc, Action<int, Exception> exceptionHandler)
     {
         var reader = new StreamReader(File.OpenRead(fileName));
         var result = new List<T>();
         var lineNumber = 0;
         
         while (!reader.EndOfStream)
+        try
         {
             if (cancellationToken.IsCancellationRequested)
                 break; 
-
             lineNumber ++;
             var line = await reader.ReadLineAsync();
             
@@ -21,9 +23,12 @@ public static class FileUtil
                 continue;
 
             var lineValues = line.Split(",");
-            var item = parseValuesFunc(lineValues, lineNumber);
-
+            var item = parseValuesFunc(lineValues);
             result.Add(item);
+        }
+        catch (Exception ex) 
+        {
+            exceptionHandler(lineNumber, ex);
         }
         return result;
     }
--- src/Vesting/Application/UseCases/ReadFile/ReadFileUseCase.cs
+++ src/Vesting/Application/UseCases/ReadFile/ReadFileUseCase.cs
@@ -13,25 +13,18 @@ public class ReadFileUseCase : IReadFileUseCase
     public async Task<ReadFileOutput> Execute(ReadFileInput input, CancellationToken cancellationToken)
     {
         var vestingEvents = await FileUtil.ReadAllLines(input.FileName, cancellationToken, 
-            (lineValues, lineNumber) => 
+            (lineValues) => new VestingEvent
             {
-                try
-                {
-                    return new VestingEvent
-                    {
-                        Type = Enum.Parse<VestingType>(lineValues[0]),
-                        EmployeeId = lineValues[1],
-                        EmployeeName = lineValues[2], 
-                        AwardId = lineValues[3],
-                        Date = DateTime.Parse(lineValues[4]),
-                        Quantity = ParseQuantity(lineValues[5], input.Digits)
-                    };
-                }
-                catch (Exception ex)
-                {
-                    throw new InvalidDataException($"ERROR on File Line {lineNumber}: {ex.Message}", ex);
-                }
-            });
+                Type = Enum.Parse<VestingType>(lineValues[0]),
+                EmployeeId = lineValues[1],
+                EmployeeName = lineValues[2], 
+                AwardId = lineValues[3],
+                Date = DateTime.Parse(lineValues[4]),
+                Quantity = ParseQuantity(lineValues[5], input.Digits)
+            },
+            (lineNumber, ex) =>
+                throw new InvalidDataException($"File Line {lineNumber} is invalid: {ex.Message}", ex)
+        );
 
         return vestingEvents.ToOutput();
     }
@@ -39,7 +32,6 @@ public class ReadFileUseCase : IReadFileUseCase
     private decimal ParseQuantity(string quantity, int digits)
     {
         var parsed = decimal.Parse(quantity, Number, InvariantCulture);
-        
         return Math.Round(parsed, digits, MidpointRounding.ToZero);
     }
 }
